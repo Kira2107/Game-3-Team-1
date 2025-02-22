0ffffe9b78ccbe39ac3f9f169f89399a0c33ded7
Kira2107 <hritish2001@gmail.com>
2025-02-22 02:35:36 -0500

    added player input system

---
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player RigidBody
+    private Rigidbody playerRb;
+
+    //Player move speed
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMove(InputValue value)
+    {
+        // Move the player
+        Vector3 moveDirection = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
+        playerRb.velocity = moveDirection * moveSpeed;
+    }
+
+}
