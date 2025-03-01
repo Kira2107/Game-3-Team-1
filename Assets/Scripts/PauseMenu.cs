de0b46e3d17e8d8832f3688525161d2c06c992a9
KMcNulty23 <kmcnulty005@gmail.com>
2025-02-28 21:15:01 -0500

    Pause menu working. Art not done yet

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button ResumeButton, MainMenuButton, settingsButton, exitButton;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject settingsMenu;
+    private bool isPaused = false;
+
+    //Start is called before the first frame update
     void Start()
     {
-        
+        ResumeButton.onClick.AddListener(Resume);
+        MainMenuButton.onClick.AddListener(MainMenu);
+        exitButton.onClick.AddListener(Exit);
+        settingsButton.onClick.AddListener(OpenSettings);
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            if (!isPaused)
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f; //Freeze game
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f; //Resume game
+        pauseMenu.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true); //Show settings
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f; //Reset time before loading menu
+        SceneManager.LoadScene(0); //Ensure Main Menu is at index 0
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
