c25857f212858d88277efe087e1361c9471aaf0d
Kira2107 <hritish2001@gmail.com>
2025-03-03 21:30:21 -0500

    Changed MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,11 +12,18 @@ public class MainMenu : MonoBehaviour
     //Start is called before the first frame update
     void Start()
     {
-        playButton.onClick.AddListener(Play);
-        exitButton.onClick.AddListener(Exit);
-        settingsButton.onClick.AddListener(OpenSettings);
-        creditsButton.onClick.AddListener(OpenCredits);
-        closeCreditsButton.onClick.AddListener(CloseCredits);
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            playButton.onClick.AddListener(Play);
+            exitButton.onClick.AddListener(Exit);
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            settingsButton.onClick.AddListener(OpenSettings);
+            creditsButton.onClick.AddListener(OpenCredits);
+            closeCreditsButton.onClick.AddListener(CloseCredits);
+        }
+        
     }
 
     //Update is called once per frame
